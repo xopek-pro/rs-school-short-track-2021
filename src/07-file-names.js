ef5a378a0a89d29ba104d7a01020c80d40880d0b
xopek-pro <xopek.pro@internet.ru>
2021-04-13 00:27:58 +0300

    feat: add function renameFiles

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,32 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const result = [];
+  for (let i = 0; i < names.length; i++) {
+    if (!result.includes(names[i])) {
+      result.push(names[i]);
+    } else {
+      let app = 0;
+      for (let j = 0; j < i; j++) {
+        if (names[i] === names[j]) {
+          app++;
+        }
+      }
+      if (app === 0) {
+        let inst = 0;
+        for (let j = 0; j < result.length; j++) {
+          if (names[i] === result[j]) {
+            inst++;
+          }
+        }
+        result.push(`${names[i]}(${inst})`);
+      } else {
+        result.push(`${names[i]}(${app})`);
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = renameFiles;
